7ac2f3d4f88c0fc2774ae6220cd2aedfa2f45ab1
aditya.singh <aditya.singh@tataiq.com>
2023-12-11 11:36:08 +0000

    .

--- preprocessing/app.py
+++ preprocessing/app.py
@@ -11,55 +11,105 @@ from src.parser import Config
 
 os.environ["SHARED_MEMORY_USE_LOCK"] = "1"
 
-def get_confdata(conf):
-    res=requests.get(conf[0]["consul_url"])
-    data=res.json()
-    dbconf =None
+def get_service_address(consul_client,service_name,env):
+    while True:
+        
+        try:
+            services=consul_client.catalog.service(service_name)[1]
+            print(services)
+            for i in services:
+                if env == i["ServiceID"].split("-")[:-1]:
+                    return i
+        except:
+            time.sleep(10)
+            continue
+def get_confdata(consul_conf):
+    consul_client = consul.Consul(host=consul_conf["host"],port=consul_conf["port"])
+    pipelineconf=get_service_address(consul_client,"pipelineconfig",consul_conf["env"])
+
     
-    preprocessconf=None
-    env=None
-    consulconf=None
-    if "pipelineconfig" in data:
-        port=data["pipelineconfig"]["Port"]
-        while True:
-            endpoints=requests.get("http://pipelineconfig.service.consul:"+str(port)+"/").json()
-            #print(endpoints)
-            if "preprocess" in endpoints["endpoint"]:
-                try:
-                    preprocessconf=requests.get("http://pipelineconfig.service.consul:"+str(port)+"/"+endpoints["endpoint"]["preprocess"]).json()
-                except Exception as ex:
-                    print(ex)
-                    time.sleep(5)
-                    continue
-            if "dbapi" in endpoints["endpoint"] and "dbapi" in data:
-                try:
-                    dbconf=requests.get("http://pipelineconfig.service.consul:"+str(port)+"/"+endpoints["endpoint"]["dbapi"]).json()
-                except Exception as ex:
-                    print(ex)
-                    time.sleep(5)
-                    continue
+    
+    env=consul_conf["env"]
+    
+    endpoint_addr="http://"+pipelineconf["ServiceAddress"]+":"+str(pipelineconf["ServicePort"])
+    print("endpoint addr====",endpoint_addr)
+    while True:
+        
+        try:
+            res=requests.get(endpoint_addr+"/")
+            endpoints=res.json()
+            print("===got endpoints===",endpoints)
+            break
+        except Exception as ex:
+            print("endpoint exception==>",ex)
+            time.sleep(10)
+            continue
+    
+    while True:
+        try:
+            res=requests.get(endpoint_addr+endpoints["endpoint"]["preprocess"])
+            preprocessconf=res.json()
+            print("preprocessconf===>",preprocessconf)
+            break
             
-            if "kafka" in endpoints["endpoint"]:
-                try:
-                    kafkaconf=requests.get("http://pipelineconfig.service.consul:"+str(port)+"/"+endpoints["endpoint"]["kafka"]).json()
-                except Exception as ex:
-                    print(ex)
-                    time.sleep(5)
-                    continue
-            if "consul" in endpoints["endpoint"]:
-                try:
-                    consulconf=requests.get("http://pipelineconfig.service.consul:"+str(port)+"/"+endpoints["endpoint"]["consul"]).json()
-                except Exception as ex:
-                    print(ex)
-                    time.sleep(5)
-                    continue
-            print(dbconf)
-            print(preprocessconf)
-            if dbconf is not None and preprocessconf is not None and kafkaconf is not None:
-                break
-    print("******")
-    print(dbconf)
-    return  dbconf,preprocessconf,kafkaconf,consulconf
+
+        except Exception as ex:
+            print("preprocessconf exception==>",ex)
+            time.sleep(10)
+            continue
+    while True:
+        try:
+            res=requests.get(endpoint_addr+endpoints["endpoint"]["kafka"])
+            kafkaconf=res.json()
+            print("kafkaconf===>",kafkaconf)
+            break
+            
+
+        except Exception as ex:
+            print("kafkaconf exception==>",ex)
+            time.sleep(10)
+            continue
+    print("=======searching for dbapi====")
+    while True:
+        try:
+            print("=====consul search====")
+            dbconf=get_service_address(consul_client,"dbapi",consul_conf["env"])
+            print("****",dbconf)
+            dbhost=dbconf["ServiceAddress"]
+            dbport=dbconf["ServicePort"]
+            res=requests.get(endpoint_addr+endpoints["endpoint"]["dbapi"])
+            dbres=res.json()
+            print("===got db conf===")
+            print(dbres)
+            break
+        except Exception as ex:
+            print("db discovery exception===",ex)
+            time.sleep(10)
+            continue
+    for i in dbres["apis"]:
+        print("====>",i)
+        dbres["apis"][i]="http://"+dbhost+":"+str(dbport)+dbres["apis"][i]
+
+    while True:
+        try:
+            print("=====consul search====")
+            ppconf=get_service_address(consul_client,"postprocess",consul_conf["env"])
+            print("****",dbconf)
+            pphost=ppconf["ServiceAddress"]
+            ppport=ppconf["ServicePort"]
+            
+            break
+        except Exception as ex:
+            print("db discovery exception===",ex)
+            time.sleep(10)
+            continue
+    
+    print("======dbres======")
+    print(dbres)
+    print(preprocessconf)
+    postprocessapi="http://"+pphost+":"+str(ppport)+preprocessconf["postprocess"]
+    return  dbres,preprocessconf,kafkaconf,postprocessapi
+
 
 preprocess_smd = SharedMemoryDict(name="preprocess", size=10000000)
 postprocess_smd = SharedMemoryDict(name="postprocess", size=10000000)
@@ -79,9 +129,9 @@ if __name__ == "__main__":
         del postprocess_smd
         del boundary_smd
         conf = Config.yamlconfig("config/config.yaml")
-        dbconf,preprocessconf,kafkaconf,consulconf=get_confdata(conf)
+        dbconf,preprocessconf,kafkaconf,postprocessapi=get_confdata(conf)
         redis_server=preprocessconf["redis"]
-        postprocess_api=dbconf["postprocessing_api"]["api"]
+        postprocess_api=postprocessapi
         # print(data[0]["kafka"])
         logg = create_rotating_log("logs/logs.log")
         cg = PoolConsumer(kafkaconf["kafka"], redis_server,postprocess_api ,logg)
